bc10b0e610e913ce28b444646738a3a89ac2208f
gbatra3 <gbatra3@wisc.edu>
2025-02-08 14:26:55 -0600

    chore: add setup and solution for part 1

---
+++ task_1/README.md
@@ -0,0 +1,23 @@
+# Part 1: Training VGG-11 on CIFAR-10
+
+## Metrics after running the CNN for one epoch with a batch size of 256 for 196 iterations
+
+Epoch [1], Iteration [20/196], Loss: 3.3479
+Epoch [1], Iteration [40/196], Loss: 2.5626
+Epoch [1], Iteration [60/196], Loss: 2.2670
+Epoch [1], Iteration [80/196], Loss: 2.3202
+Epoch [1], Iteration [100/196], Loss: 2.2707
+Epoch [1], Iteration [120/196], Loss: 2.2434
+Epoch [1], Iteration [140/196], Loss: 2.2375
+Epoch [1], Iteration [160/196], Loss: 2.2207
+Epoch [1], Iteration [180/196], Loss: 2.2134
+Epoch [1] completed. Average time per iteration (after discarding first): 2.152791 seconds
+Epoch [1] Training complete. Average Loss: 2.7683, Accuracy: 12.40%
+Test set: Average loss: 2.1665, Accuracy: 1674/10000 (17%)
+
+## Metrics after running the CNN for one epoch with a batch size of 256 for 40 iterations
+
+Epoch [1], Iteration [20/196], Loss: 2.8673
+Epoch [1], Iteration [40/196], Loss: 2.3245
+Epoch [1] completed. Average time per iteration (after discarding first): 2.336988 seconds
+Epoch [1] Training complete. Average Loss: 0.9930, Accuracy: 11.51%
---
+++ task_1/main.py
@@ -0,0 +1,154 @@
+import torch
+from torch.nn.modules import CrossEntropyLoss
+from torch.utils.data.dataloader import DataLoader
+from torchvision import datasets, transforms
+import torch.nn as nn
+import torch.optim as optim
+import model as mdl
+import time
+
+
+device = "cpu"
+torch.set_num_threads(4)
+
+batch_size = 256  # batch for one node
+
+
+def train_model(
+    model: nn.Module,
+    train_loader: DataLoader,
+    optimizer: optim.Optimizer,
+    criterion: CrossEntropyLoss,
+    epoch: int,
+):
+    """
+    model (torch.nn.module): The model created to train
+    train_loader (pytorch data loader): Training data loader
+    optimizer (optimizer.*): A instance of some sort of optimizer, usually SGD
+    criterion (nn.CrossEntropyLoss) : Loss function used to train the network
+    epoch (int): Current epoch number
+    """
+    model.train()  ### just a good practice, only necessary if we have Droupout/BatchNorm layers
+    running_loss: float = 0.0
+    correct: int = 0
+    total: int = 0
+
+    times = []
+
+    # remember to exit the train loop at end of the epoch
+    for batch_idx, (data, target) in enumerate(train_loader):
+        # Your code goes here!
+        if batch_idx >= 40:
+            break  # NOTE: Since shuffle is true we get different samples
+
+        start_time = time.time()
+
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output: torch.Tensor = model(data)
+        loss: torch.Tensor = criterion(output, target)
+
+        loss.backward()
+        optimizer.step()
+
+        end_time = time.time()
+        iteration_time = end_time - start_time
+
+        times.append(iteration_time)
+
+        running_loss += loss.item()
+
+        _, predicted = output.max(1)
+        correct += int(predicted.eq(target).sum().item())
+        total += target.size(0)
+
+        if (batch_idx + 1) % 20 == 0:
+            print(
+                f"Epoch [{epoch+1}], Iteration [{batch_idx+1}/{len(train_loader)}], Loss: {loss.item():.4f}"
+            )
+
+    avg_time = sum(times[1:]) / len(times[1:]) if len(times) > 1 else 0.0
+    print(
+        f"Epoch [{epoch+1}] completed. Average time per iteration (after discarding first): {avg_time:.6f} seconds"
+    )
+
+    avg_loss = running_loss / len(train_loader)
+    accuracy = 100 * correct / total
+    print(
+        f"Epoch [{epoch+1}] Training complete. Average Loss: {avg_loss:.4f}, Accuracy: {accuracy:.2f}%"
+    )
+
+
+def test_model(
+    model: nn.Module,
+    test_loader: DataLoader,
+    criterion: CrossEntropyLoss,
+):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target)
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader)
+    print(
+        "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss,
+            correct,
+            len(test_loader.dataset),
+            100.0 * correct / len(test_loader.dataset),
+        )
+    )
+
+
+def main():
+    normalize = transforms.Normalize(
+        mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
+        std=[x / 255.0 for x in [63.0, 62.1, 66.7]],
+    )
+    transform_train = transforms.Compose(
+        [
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]
+    )
+
+    transform_test = transforms.Compose([transforms.ToTensor(), normalize])
+    training_set = datasets.CIFAR10(
+        root="./data", train=True, download=True, transform=transform_train
+    )
+    train_loader: DataLoader = DataLoader(
+        training_set,
+        num_workers=2,
+        batch_size=batch_size,
+        sampler=None,
+        shuffle=True,
+        pin_memory=True,
+    )
+    test_set = datasets.CIFAR10(
+        root="./data", train=False, download=True, transform=transform_test
+    )
+
+    test_loader: DataLoader = DataLoader(
+        test_set, num_workers=2, batch_size=batch_size, shuffle=False, pin_memory=True
+    )
+    training_criterion = CrossEntropyLoss().to(device)
+
+    model = mdl.VGG11()
+    model.to(device)
+    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0001)
+    # running training for one epoch
+    for epoch in range(1):
+        train_model(model, train_loader, optimizer, training_criterion, epoch)
+        test_model(model, test_loader, training_criterion)
+
+
+if __name__ == "__main__":
+    main()
