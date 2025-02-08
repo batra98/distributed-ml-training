15714735a6289aa2626b992e54a99209f03eb3a1
gbatra3 <gbatra3@wisc.edu>
2025-02-08 16:48:21 -0600

    feat: added code for part 3
    - used Distributed Data Sampler to distribute data.
    - init_process_group to ininalize the setup using local ranks and world
    size
    - DDP on the model to broadcast and later gather

---
+++ task_3/main.py
@@ -0,0 +1,218 @@
+from typing import cast
+import torch
+from torch.nn.modules import CrossEntropyLoss
+from torch.utils.data import DataLoader, DistributedSampler
+from torchvision import datasets, transforms
+import torch.nn as nn
+from torch.nn.parallel import DistributedDataParallel
+import torch.optim as optim
+import model as mdl
+import time
+import argparse
+import numpy as np
+import torch.distributed as dist
+
+device = "cpu"
+torch.set_num_threads(4)
+
+batch_size = 256  # batch for one node
+seed = 42
+
+
+def init_distributed_mode(args):
+    if args.rank == 0:
+        print("Initializing distributed mode...")
+
+    dist.init_process_group(
+        backend="gloo",
+        init_method=f"tcp://{args.master_ip}:29500",
+        world_size=args.num_nodes,
+        rank=args.rank,
+    )
+
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+
+def train_model(
+    model: nn.Module,
+    train_loader: DataLoader,
+    optimizer: optim.Optimizer,
+    criterion: CrossEntropyLoss,
+    epoch: int,
+):
+    """
+    model (torch.nn.module): The model created to train
+    train_loader (pytorch data loader): Training data loader
+    optimizer (optimizer.*): A instance of some sort of optimizer, usually SGD
+    criterion (nn.CrossEntropyLoss) : Loss function used to train the network
+    epoch (int): Current epoch number
+    """
+    model.train()  ### just a good practice, only necessary if we have Droupout/BatchNorm layers
+    running_loss: float = 0.0
+    correct: int = 0
+    total: int = 0
+
+    times = []
+
+    # remember to exit the train loop at end of the epoch
+    for batch_idx, (data, target) in enumerate(train_loader):
+        # Your code goes here!
+        if batch_idx >= 40:
+            break  # NOTE: Since shuffle is true we get different samples
+
+        start_time = time.time()
+
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output: torch.Tensor = model(data)
+        loss: torch.Tensor = criterion(output, target)
+
+        loss.backward()
+        optimizer.step()
+
+        end_time = time.time()
+        iteration_time = end_time - start_time
+
+        times.append(iteration_time)
+
+        running_loss += loss.item()
+
+        _, predicted = output.max(1)
+        correct += int(predicted.eq(target).sum().item())
+        total += target.size(0)
+
+        if (batch_idx + 1) % 20 == 0:
+            print(
+                f"Epoch [{epoch+1}], Iteration [{batch_idx+1}/{len(train_loader)}], Loss: {loss.item():.4f}"
+            )
+
+    avg_time = sum(times[1:]) / len(times[1:]) if len(times) > 1 else 0.0
+    print(
+        f"Epoch [{epoch+1}] completed. Average time per iteration (after discarding first): {avg_time:.6f} seconds"
+    )
+
+    avg_loss = running_loss / len(train_loader)
+    accuracy = 100 * correct / total
+    print(
+        f"Epoch [{epoch+1}] Training complete. Average Loss: {avg_loss:.4f}, Accuracy: {accuracy:.2f}%"
+    )
+
+
+def test_model(
+    model: nn.Module,
+    test_loader: DataLoader,
+    criterion: CrossEntropyLoss,
+):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target)
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader)
+    print(
+        "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss,
+            correct,
+            len(test_loader.dataset),
+            100.0 * correct / len(test_loader.dataset),
+        )
+    )
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--master-ip", type=str, help="IP address of the master node")
+    parser.add_argument(
+        "--num-nodes", type=int, help="Total number of nodes in the distributed setup"
+    )
+    parser.add_argument(
+        "--rank", type=int, help="Rank of the current node (starting from 0)"
+    )
+    args = parser.parse_args()
+
+    if args.master_ip and args.num_nodes and args.rank is not None:
+        init_distributed_mode(args)
+        distributed = True
+    else:
+        print("Running in single CPU mode.")
+        distributed = False
+
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    normalize = transforms.Normalize(
+        mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
+        std=[x / 255.0 for x in [63.0, 62.1, 66.7]],
+    )
+
+    transform_train = transforms.Compose(
+        [
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]
+    )
+
+    transform_test = transforms.Compose([transforms.ToTensor(), normalize])
+
+    training_set = datasets.CIFAR10(
+        root="./data", train=True, download=True, transform=transform_train
+    )
+    test_set = datasets.CIFAR10(
+        root="./data", train=False, download=True, transform=transform_test
+    )
+
+    if distributed:
+        train_sampler = DistributedSampler(
+            training_set, num_replicas=args.num_nodes, rank=args.rank
+        )
+        train_loader = DataLoader(
+            training_set,
+            batch_size=batch_size,
+            sampler=train_sampler,
+            num_workers=2,
+            pin_memory=True,
+        )
+    else:
+        train_sampler = None
+        train_loader = DataLoader(
+            training_set,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=2,
+            pin_memory=True,
+        )
+
+    test_loader = DataLoader(
+        test_set, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True
+    )
+
+    model = mdl.VGG11().to(device)
+
+    if distributed:
+        model = DistributedDataParallel(model)
+
+    criterion = CrossEntropyLoss().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0001)
+
+    # Training loop
+    for epoch in range(1):
+        if distributed:
+            train_sampler = cast(
+                DistributedSampler, train_sampler
+            )  # Just to remove typing error
+            train_sampler.set_epoch(epoch)
+        train_model(model, train_loader, optimizer, criterion, epoch)
+        test_model(model, test_loader, criterion)
+
+
+if __name__ == "__main__":
+    main()
